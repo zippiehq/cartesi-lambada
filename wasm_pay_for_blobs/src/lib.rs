f30106bb32cd69fe29a4d746100777e9af1902a4
Yelyzaveta Dymchenko <yelyzaveta.dymchenko@zippie.com>
2024-02-13 10:26:41 +0000

    feat: add encode_blob_tx function

--- wasm_pay_for_blobs/src/lib.rs
+++ wasm_pay_for_blobs/src/lib.rs
@@ -1,21 +1,32 @@
 use celestia_proto::celestia::blob::v1::MsgPayForBlobs;
 use celestia_proto::cosmos::tx::v1beta1::TxBody;
-use prost::Message;
-use wasm_bindgen::prelude::wasm_bindgen;
-use celestia_types::Commitment;
+use celestia_tendermint_proto::v0_34::types::{Blob, BlobTx};
 use celestia_types::consts::appconsts;
 use celestia_types::nmt::Namespace;
+use celestia_types::Commitment;
+use prost::Message;
 use prost_types::Any;
+use wasm_bindgen::prelude::wasm_bindgen;
+
 #[wasm_bindgen]
-pub fn pay_blobs(signer_address: &str, vm_id_namespace: Vec<u8>, data: Vec<u8>, share_version: u32) -> Vec<u8> {
-    let commitment =
-            Commitment::from_blob(Namespace::from_raw(&vm_id_namespace).unwrap(), appconsts::SHARE_VERSION_ZERO, &data[..]).expect("commitment receiving failed");
-    let msg = MsgPayForBlobs{
+pub fn pay_blobs(
+    signer_address: &str,
+    vm_id_namespace: Vec<u8>,
+    data: Vec<u8>,
+    share_version: u32,
+) -> Vec<u8> {
+    let commitment = Commitment::from_blob(
+        Namespace::from_raw(&vm_id_namespace).unwrap(),
+        appconsts::SHARE_VERSION_ZERO,
+        &data[..],
+    )
+    .expect("commitment receiving failed");
+    let msg = MsgPayForBlobs {
         signer: signer_address.to_string(),
         namespaces: vec![vm_id_namespace],
         blob_sizes: vec![data.len() as u32],
         share_commitments: vec![commitment.0.to_vec()],
-        share_versions: vec![share_version]
+        share_versions: vec![share_version],
     };
     let mut buf = Vec::new();
     msg.encode(&mut buf).expect("encoding failed");
@@ -23,17 +34,29 @@ pub fn pay_blobs(signer_address: &str, vm_id_namespace: Vec<u8>, data: Vec<u8>, 
 }
 
 #[wasm_bindgen]
-pub fn message_to_tx(velue: Vec<u8>) -> Vec<u8>{
+pub fn message_to_tx(value: Vec<u8>) -> Vec<u8> {
     let any_msg = Any {
         type_url: "/celestia.blob.v1.MsgPayForBlobs".to_string(),
-        value: velue, 
+        value: value,
     };
 
     let tx_body = TxBody {
-        messages: vec![any_msg], 
-        ..Default::default() 
+        messages: vec![any_msg],
+        ..Default::default()
     };
 
     let mut buf = Vec::new();
     tx_body.encode(&mut buf).expect("encoding failed");
-    buf}
+    buf
+}
+
+pub fn encode_blob_tx(tx: Vec<u8>, encoded_blobs: Vec<Vec<u8>>, type_id: String) -> Vec<u8> {
+    let blobs: Vec<Blob> = encoded_blobs
+        .iter()
+        .map(|encoded_blob| Blob::decode(encoded_blob as &[u8]).unwrap())
+        .collect();
+    let blob_tx = BlobTx { tx, blobs, type_id };
+    let mut buf = Vec::new();
+    blob_tx.encode(&mut buf).expect("encoding failed");
+    buf
+}
